a5a546242fe994f2c1cc025b4571e5fa4cce8d54
Anderson Freitas <alsfreitas@mars>
2015-01-15 15:38:00 -0200

    acertada a renderização dos hints

--- bootstrap.js
+++ bootstrap.js
@@ -1,33 +1,47 @@
-ESCALA_ZOOM = [1, 16]
-
-var myFormatters = d3.locale({
-  "decimal": ",",
-  "thousands": ".",
-  "grouping": [3],
-  "currency": ["R$", ""],
-  "dateTime": "%a %b %e %X %Y",
-  "date": "%m/%d/%Y",
-  "time": "%H:%M:%S",
-  "periods": ["AM", "PM"],
-  "days": ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
-  "shortDays": ["Dom", "Seg", "Ter", "Qua", "qui", "Sex", "Sab"],
-  "months": ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembo", "Outubro", "Novembro", "Dezembro"],
-  "shortMonths": ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dec"]
-});
-
-
-d3.format = myFormatters.numberFormat;
-numero = d3.format(",.$")
-
-var margin = {top: 20, right: 20, bottom: 50, left: 20};
-var WIDTH = 600 - margin.left - margin.right;
-var HEIGHT = 160 - margin.top - margin.bottom;
-var HEIGHT_X_AXIS = 0.8*HEIGHT
+Array.prototype.unique = function()
+{
+  var n = {},r=[];
+  for(var i = 0; i < this.length; i++)
+  {
+    if (!n[this[i].cx])
+    {
+      n[this[i].cx] = true;
+      r.push(this[i]);
+    }
+  }
+  return r;
+}
 
 var drawD3Document = function (retorno, canvas, indice){
 
+  var ESCALA_ZOOM = [1, 16]
+
+  var myFormatters = d3.locale({
+    "decimal": ",",
+    "thousands": ".",
+    "grouping": [3],
+    "currency": ["R$", ""],
+    "dateTime": "%a %b %e %X %Y",
+    "date": "%m/%d/%Y",
+    "time": "%H:%M:%S",
+    "periods": ["AM", "PM"],
+    "days": ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+    "shortDays": ["Dom", "Seg", "Ter", "Qua", "qui", "Sex", "Sab"],
+    "months": ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembo", "Outubro", "Novembro", "Dezembro"],
+    "shortMonths": ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dec"]
+  });
+
+
+  d3.format = myFormatters.numberFormat;
+  var numero = d3.format(",.$")
+
+  var margin = {top: 20, right: 20, bottom: 50, left: 20};
+  var WIDTH = 600 - margin.left - margin.right;
+  var HEIGHT = 160 - margin.top - margin.bottom;
+  var HEIGHT_X_AXIS = 0.8*HEIGHT
+
   var numOfticks = 20
-  var rect = {"x":retorno.limitesBootstrap[0], "y":30, "height":30, "x2":retorno.limitesBootstrap[1], "fill":"yellow", "opacity":0.5};
+  var rect = {"x":retorno.limitesBootstrap[0]+5000, "y":30, "height":30, "x2":retorno.limitesBootstrap[1]+15000, "fill":"yellow", "opacity":0.5};
   var data = retorno.pontos
 
   data.forEach(function(d) {
@@ -35,16 +49,31 @@ var drawD3Document = function (retorno, canvas, indice){
     d.cy = 20;
     d.color = "blue";
   });
-  data[data.length-1].color = "red"
+
+  data.push({'cx': $("#"+canvas).attr('data-custo-item'), 'cy': 20, 'color': "red"})
+
+  console.log(data)
 
   data.sort(function(a,b){
     return a.cx - b.cx;
   });
 
+  var unicos = data.unique()
+  for(var i = 0; i<unicos.length; i++){
+    for(var j = 0; j<data.length; j++){
+      if(unicos[i].cx == data[j].cx){
+        if(!unicos[i].html){
+          unicos[i].html = "CUSTO: "+numero(data[j].cx)+"<br/>"
+        }
+        if(data[j].nomeProjeto  || data[j].salic || data[j].salic){
+          unicos[i].html += "<br/>PROJETO: "+data[j].nomeProjeto+"<br/>"+"SALIC: "+data[j].salic+"<br/>"+"FSA: "+data[j].fsa+"<br/>";
+        }
+      }
+    }
+  }
+
   var xScale = d3.scale.linear().domain([data[0].cx, data[data.length-1].cx]).range([0, WIDTH]);
   var xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(numOfticks).tickSize(-HEIGHT_X_AXIS/8);
-
-  console.log(xScale)
 
   var zoom = d3.behavior.zoom()
   .x(xScale)
@@ -92,7 +121,12 @@ var drawD3Document = function (retorno, canvas, indice){
   .attr('height', HEIGHT_X_AXIS)
   .attr('fill', "blue")
   .attr('opacity', 0)
-  .attr("class", "rect1");
+  .attr("class", "rect1")
+   .on("click", function(d) {
+    div.transition()
+    .duration(500)
+    .style("opacity", 0);
+  });
 
   svg.append("rect")
   .attr('x', function(d){return xScale(+rect.x)})
@@ -101,10 +135,15 @@ var drawD3Document = function (retorno, canvas, indice){
   .attr('height', rect.height)
   .attr('fill', rect.fill)
   .attr('opacity', rect.opacity)
-  .attr("class", "rect2");
+  .attr("class", "rect2")
+  .on("click", function(d) {
+    div.transition()
+    .duration(500)
+    .style("opacity", 0);
+  });
 
   svg.selectAll("circle")
-  .data(data)
+  .data(unicos)
   .enter()
   .append("circle")
   .attr("cx", function(d){return xScale(d.cx)})
@@ -113,14 +152,14 @@ var drawD3Document = function (retorno, canvas, indice){
   .attr("class", "circle"+indice)
   .style("fill", function(d){return d.color})
   .on("mouseover", function(d) {
-    div.transition()
+    div.html(d.html)
+    .transition()
     .duration(200)
-    .style("opacity", .9);
-    div.html("SALIC: "+d.salic+"<br/>"+"PROJETO: "+d.nomeProjeto+"<br/>"+"VALOR: "+numero(d.cx)+"<br/>")
+    .style("opacity", .9)
     .style("left", (d3.event.pageX) + "px")
     .style("top", (d3.event.pageY - 40) + "px");
   })
-  .on("mouseout", function(d) {
+  .on("click", function(d) {
     div.transition()
     .duration(500)
     .style("opacity", 0);
@@ -144,9 +183,6 @@ var drawD3Document = function (retorno, canvas, indice){
     }else if(tx < scrollMaxDaDireita){
         tx = scrollMaxDaDireita
     }
-
-    // console.log("scrollMaxParaDireita: " + scrollMaxDaDireita + "   scrollmaxEsquerda: " + scrollMaxDaEsquerda)
-    // console.log("tx: " + zoom.translate()[0])
 
     svg.select("#x"+indice).call(xAxis).attr("transform", "translate(" + tx + ", "+HEIGHT_X_AXIS+")scale(1)")
     .selectAll("text")
